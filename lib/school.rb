6ce7089a730be8c5ee5e57c65bc731537043a1ce
Michael Morris <mdam2345@gmail.com>
2018-06-30 03:39:44 +0000

    automatically backed up by learn

--- lib/school.rb
+++ lib/school.rb
@@ -6,7 +6,13 @@ class School
   end
 
   def add_student(student_name, grade)
+    if @roster[grade]
       @roster[grade] << student_name
+    else
+      @roster[grade] = []
+      @roster[grade] << student_name
+    end
+  end
 
   end
 end
