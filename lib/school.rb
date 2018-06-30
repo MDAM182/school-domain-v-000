e29c83a4dab0cb58773faf5b190fa4a3486d2154
Michael Morris <mdam2345@gmail.com>
2018-06-30 03:49:58 +0000

    automatically backed up by learn

--- lib/school.rb
+++ lib/school.rb
@@ -13,15 +13,14 @@ class School
       @roster[grade] << student_name
     end
   end
-  def grade(student_grade) #part 3
+  def grade(student_grade) 
      roster[student_grade]
    end
 
-   def sort  #part 4
-     sorted = {} 
-     roster.each do |grade, students| #iterate through each grade and student
-       sorted[grade] = students.sort #grade is the key in the sorted hash. Value is the students sorted in ABC order.
+   def sort  
+     sorted = {}
+     roster.each do |grade, students| 
+       sorted[grade] = students.sort 
      end
-     sorted #the new sorted roster.
-   end
+     sorted 
  end
